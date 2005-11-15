2e7078f691fcd39087181e5f16b2e5f9307a6011
Nicholas J Humfrey <njh@aelius.com>
2005-11-15 00:32:35 +0000

    modernised bootstrap script a bit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,67 @@
 #!/bin/sh
-#
+# Run this to set up the build system: configure, makefiles, etc.
+
+package="MAST"
+
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+cd "$srcdir"
+DIE=0
+
+(autoheader --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have autoconf installed to compile $package."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    DIE=1
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have autoconf installed to compile $package."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have automake installed to compile $package."
+    echo "Download the appropriate package for your system,"
+    echo "or get the source from one of the GNU ftp sites"
+    echo "listed in http://www.gnu.org/order/ftp.html"
+    DIE=1
+}
+
+(pkg-config --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have pkg-config installed to compile $package."
+    echo "Download the appropriate package for your system,"
+    echo "or get the source from http://pkgconfig.freedesktop.org/"
+    DIE=1
+}
+
+
+if test "$DIE" -eq 1; then
+    exit 1
+fi
+
+
+
+echo "Generating configuration files for $package, please wait...."
+
+# Create the (empty) build directory if it doesn't exist
+if ! [ -d build ]; then
+	echo "  creating build directory"
+	mkdir build
+fi
+
+
 
 run_cmd() {
-    echo running $* ...
+    echo "  running $* ..."
     if ! $*; then
 			echo failed!
 			exit 1
@@ -15,6 +74,5 @@ run_cmd autoheader
 run_cmd automake --add-missing --copy
 run_cmd autoconf
 
-echo
-echo "Now type './configure' to configure MAST"
-echo
+
+$srcdir/configure --enable-debug && echo
