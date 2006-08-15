5a9c651d30129a93affc535507dbb6e7c35d47db
Nicholas J Humfrey <njh@aelius.com>
2006-08-15 21:30:58 +0000

    Common include file for all tools.

---
+++ src/mast.h
@@ -0,0 +1,53 @@
+/*
+ *  MAST: Multicast Audio Streaming Toolkit
+ *
+ *  By Nicholas J. Humfrey <njh@ecs.soton.ac.uk>
+ *
+ *  Copyright (C) 2003-2006 University of Southampton
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  
+ */
+
+
+#ifndef	_MAST_H_
+#define	_MAST_H_
+
+
+// Use by gethostname()
+#ifndef HOST_NAME_MAX
+#ifdef _POSIX_HOST_NAME_MAX
+#define HOST_NAME_MAX	_POSIX_HOST_NAME_MAX
+#else
+#define HOST_NAME_MAX	(256)
+#endif
+#endif
+
+#ifndef DOMAIN_NAME_MAX
+#define DOMAIN_NAME_MAX	(1024)
+#endif
+
+// Standard string buffer size
+#define STR_BUF_SIZE			1025
+
+
+
+
+// In gethostname.c
+extern char* gethostname_fqdn();
+
+
+
+#endif
