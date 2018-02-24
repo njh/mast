52f7004d63680a5195b76b90bf01664889ca423b
Nicholas Humfrey <njh@aelius.com>
2018-02-24 23:29:50 +0000

    Create the build-scripts directory, if it doesn't exist

--- autogen.sh
+++ autogen.sh
@@ -69,6 +69,11 @@ run_cmd() {
 }
 
 
+# Because git doesn't support empty directories
+if [ ! -d "$srcdir/build-scripts" ]; then
+	mkdir "$srcdir/build-scripts"
+fi
+
 run_cmd aclocal
 run_cmd autoheader
 run_cmd automake --add-missing --copy
